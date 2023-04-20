7949dc76df68d14e2b87eef6ac2e558adc709b9b
ArthasGAO <91470426+ArthasGAO@users.noreply.github.com>
2023-04-20 11:28:44 +0200

    Update UserRepository.java
    Extend two query functions using SQL to satisfy Get, Put and Delete mapping needs.

--- src/main/java/ch/uzh/ifi/hase/soprafs23/repository/UserRepository.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/repository/UserRepository.java
@@ -4,6 +4,8 @@ import ch.uzh.ifi.hase.soprafs23.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import org.springframework.data.repository.query.Param;
+
 @Repository("userRepository")
 public interface UserRepository extends JpaRepository<User, Long> {
   User findByName(String name);
@@ -13,4 +15,15 @@ public interface UserRepository extends JpaRepository<User, Long> {
   User findByUsernameAndPassword(String username, String password);
 
   User findById(long id);
+    
+    
+  @Query("SELECT u FROM User u WHERE u.id = :userId")
+    User findUserByUserId(@Param("userId") Integer userId);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE User SET password=:newPassword WHERE id>:id")
+    void updatePassword(@Param("newPassword") String newPassword, @Param("id") Integer id);
+
+    void deleteById(Integer id);
 }
