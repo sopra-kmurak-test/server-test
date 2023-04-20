ba2bf535aef8c7a9aa314b169ab8470425fcca13
ArthasGAO <91470426+ArthasGAO@users.noreply.github.com>
2023-04-20 11:31:58 +0200

    Create AnswerRepository.java
    Create the AnswerRepository and the relevant functions to faciliate the Post, Get, Put and Delete mappings from controller.

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/repository/AnswerRepository.java
@@ -0,0 +1,32 @@
+package com.example.accessingdatamysql;
+
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.accessingdatamysql.User;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository
+public interface AnswerRepository extends JpaRepository<Answer, Integer> {
+    @Query("SELECT COUNT(*) FROM Answer WHERE user.id =:userId AND question.id =:questionId")
+    Integer findByUserAndQuestion(@Param("userId")Integer userId, @Param("questionId")Integer questionId);
+
+//    @Query(value = "SELECT * FROM answer WHERE question_id = :questionId", nativeQuery = true)
+//    List<Answer> findByQuestionId(@Param("questionId") Integer questionId);
+
+    List<Answer> findByQuestionId(Integer questionId);
+
+    List<Answer> findByUserId(Integer userId);
+
+    Answer findAnswerById(Integer answerId);
+
+    void deleteById(Integer id);
+
+    @Query("SELECT a.question FROM Answer a WHERE a.id = :answerId")
+    Question findQuestionByAnswerId(@Param("answerId") Integer answerId);
+}
